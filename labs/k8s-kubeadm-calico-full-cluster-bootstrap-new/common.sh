7829f7a10ed4e61b78f7313d54d200632cb1132b
Artur Scheiner <artur.scheiner@gmail.com>
2019-11-10 08:20:20 -0300

    adding ********* and step descriptions

--- labs/k8s-kubeadm-calico-full-cluster-bootstrap-new/common.sh
+++ labs/k8s-kubeadm-calico-full-cluster-bootstrap-new/common.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env bash
-
+echo **********
+echo **********
 echo ********** Adding Kubernetes Repo
+echo **********
+echo **********
 echo "deb  http://apt.kubernetes.io/  kubernetes-xenial  main" > /etc/apt/sources.list.d/kubernetes.list
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+
+echo **********
+echo **********
 echo ********** Updating Repo
+echo **********
+echo **********
 apt-get update
+
+echo **********
+echo **********
 echo ********** Upgrading Packages
+echo **********
+echo **********
 apt-get upgrade -y
+
+echo **********
+echo **********
 echo ********** Installing Required & Recommended Packages
+echo **********
+echo **********
 apt-get install -y avahi-daemon libnss-mdns traceroute htop httpie bash-completion docker.io kubeadm kubelet kubectl
