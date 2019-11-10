b656a9204b83a459de9fb4ca654d2c172fbfbff4
Artur Scheiner <artur.scheiner@gmail.com>
2019-11-10 13:10:49 -0300

    docker install adjustments

--- labs/k8s-kubeadm-calico-full-cluster-bootstrap-new/common.sh
+++ labs/k8s-kubeadm-calico-full-cluster-bootstrap-new/common.sh
@@ -18,35 +18,11 @@ add-apt-repository \
   $(lsb_release -cs) \
   stable"
 
-## Install Docker CE.
-apt-get update && apt-get install docker-ce
-
-# Setup daemon.
-cat > /etc/docker/daemon.json <<EOF
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2"
-}
-EOF
-
-#mkdir -p /etc/systemd/system/docker.service.d
-
-systemctl enable docker.service
-
-# Restart docker.
-systemctl daemon-reload
-systemctl restart docker
-
 echo "********** $KVMSG"
 echo "********** $KVMSG"
 echo "********** $KVMSG ->> Adding Kubernetes Repo"
 echo "********** $KVMSG"
 echo "********** $KVMSG"
-apt-get update && apt-get install apt-transport-https ca-certificates curl software-properties-common
 echo "deb  http://apt.kubernetes.io/  kubernetes-xenial  main" > /etc/apt/sources.list.d/kubernetes.list
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
@@ -69,4 +45,24 @@ echo "********** $KVMSG"
 echo "********** $KVMSG ->> Installing Required & Recommended Packages"
 echo "********** $KVMSG"
 echo "********** $KVMSG"
-apt-get install -y avahi-daemon libnss-mdns traceroute htop httpie bash-completion kubeadm kubelet kubectl
+apt-get install -y avahi-daemon libnss-mdns traceroute htop httpie bash-completion docker-ce kubeadm kubelet kubectl
+
+# Setup daemon.
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+#mkdir -p /etc/systemd/system/docker.service.d
+
+systemctl enable docker.service
+
+# Restart docker.
+systemctl daemon-reload
+systemctl restart docker
