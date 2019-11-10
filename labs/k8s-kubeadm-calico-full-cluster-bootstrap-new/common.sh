24763bc5bf163b792cddfc44c9f3c446884996e8
Artur Scheiner <artur.scheiner@gmail.com>
2019-11-10 10:40:10 -0300

    various scripting adjustments

--- labs/k8s-kubeadm-calico-full-cluster-bootstrap-new/common.sh
+++ labs/k8s-kubeadm-calico-full-cluster-bootstrap-new/common.sh
@@ -1,30 +1,33 @@
 #!/usr/bin/env bash
-VAR=$1
-echo ********** $VAR
-echo ********** $VAR
-echo ********** Adding Kubernetes Repo
-echo **********
-echo **********
+
+#variable definitions
+KVMSG=$1
+
+echo ********** $KVMSG
+echo ********** $KVMSG
+echo ********** $KVMSG ->> Adding Kubernetes Repo
+echo ********** $KVMSG
+echo ********** $KVMSG
 echo "deb  http://apt.kubernetes.io/  kubernetes-xenial  main" > /etc/apt/sources.list.d/kubernetes.list
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
-echo **********
-echo **********
-echo ********** Updating Repo
-echo **********
-echo **********
+echo ********** $KVMSG
+echo ********** $KVMSG
+echo ********** $KVMSG ->> Updating Repo
+echo ********** $KVMSG
+echo ********** $KVMSG
 apt-get update
 
-echo **********
-echo **********
-echo ********** Upgrading Packages
-echo **********
-echo **********
+echo ********** $KVMSG
+echo ********** $KVMSG
+echo ********** $KVMSG ->> Upgrading Packages
+echo ********** $KVMSG
+echo ********** $KVMSG
 apt-get upgrade -y
 
-echo **********
-echo **********
-echo ********** Installing Required & Recommended Packages
-echo **********
-echo **********
+echo ********** $KVMSG
+echo ********** $KVMSG
+echo ********** $KVMSG ->> Installing Required & Recommended Packages
+echo ********** $KVMSG
+echo ********** $KVMSG
 apt-get install -y avahi-daemon libnss-mdns traceroute htop httpie bash-completion docker.io kubeadm kubelet kubectl
--- labs/k8s-kubeadm-calico-full-cluster-bootstrap-new/masters.sh
+++ labs/k8s-kubeadm-calico-full-cluster-bootstrap-new/masters.sh
@@ -1,29 +1,41 @@
 #!/usr/bin/env bash
-echo **********
-echo **********
-echo ********** Adding Kubernetes Repo
-echo **********
-echo **********
-echo "deb  http://apt.kubernetes.io/  kubernetes-xenial  main" > /etc/apt/sources.list.d/kubernetes.list
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
-echo **********
-echo **********
-echo ********** Updating Repo
-echo **********
-echo **********
-apt-get update
+KVMSG=$1
+NODE=$2
+POD_CIDR=$3
+API_ADV_ADDRESS=$4
 
-echo **********
-echo **********
-echo ********** Upgrading Packages
-echo **********
-echo **********
-apt-get upgrade -y
+echo ********** $KVMSG
+echo ********** $KVMSG
+echo ********** $KVMSG ->> Initializing Kubernetes Cluster
+echo ********** $KVMSG ->> Master Node $NODE
+echo ********** $KVMSG ->> kv-master-$NODE
+kubeadm init --pod-network-cidr $POD_CIDR --apiserver-advertise-address $API_ADV_ADDRESS | tee /vagrant/kubeadm-init.out
 
-echo **********
-echo **********
-echo ********** Installing Required & Recommended Packages
-echo **********
-echo **********
-apt-get install -y avahi-daemon libnss-mdns traceroute htop httpie bash-completion docker.io kubeadm kubelet kubectl
+echo ********** $KVMSG
+echo ********** $KVMSG
+echo ********** $KVMSG ->> Configuring Kubernetes Cluster Environment
+echo ********** $KVMSG
+echo ********** $KVMSG
+mkdir -p /home/vagrant/.kube
+cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
+chown vagrant:vagrant /home/vagrant/.kube/config
+mkdir -p /root/.kube
+cp -i /etc/kubernetes/admin.conf /root/.kube/config
+
+#Configure the Calico Network Plugin
+echo ********** $KVMSG
+echo ********** $KVMSG
+echo ********** $KVMSG ->> Configuring Kubernetes Cluster Calico Networking
+echo ********** $KVMSG ->> Downloading Calico YAML File
+echo ********** $KVMSG
+echo ********** $KVMSG
+wget https://bit.ly/kv-lab-k8s-calico-yaml -O /tmp/calico-default.yaml
+sed "s+192.168.0.0/16+$POD_CIDR+g" /tmp/calico-default.yaml > /tmp/calico-defined.yaml
+
+echo ********** $KVMSG ->> Applying Calico YAML File
+echo ********** $KVMSG
+echo ********** $KVMSG
+kubectl apply -f /tmp/calico-defined.yaml
+rm /tmp/calico-default.yaml /tmp/calico-defined.yaml
+echo KUBELET_EXTRA_ARGS=--node-ip=10.8.8.1$NODE > /etc/default/kubelet
