078d494ef77355964f010593f21b8a81ca3b555f
Artur Scheiner <artur.scheiner@gmail.com>
2019-11-27 22:50:39 -0300

    worker adjustments

--- labs/kv-k8s-cluster-ha/worker.sh
+++ labs/kv-k8s-cluster-ha/worker.sh
@@ -7,24 +7,11 @@ KVMSG=$1
 NODE=$2
 WORKER_IP=$3
 MASTER_TYPE=$4
-# POD_CIDR=$3
-# API_ADV_ADDRESS=$4
-
-echo "********** $KVMSG"
-echo "********** $KVMSG"
-echo "********** $KVMSG ->> Joining Kubernetes Cluster"
-echo "********** $KVMSG ->> Worker Node $NODE"
-echo "********** $KVMSG ->> kv-worker-$NODE"
-
-# Extract and execute the kubeadm join command from the exported file
-#$(cat /vagrant/kubeadm-init.out | grep -A 2 "kubeadm join" | sed -e 's/^[ \t]*//' | tr '\n' ' ' | sed -e 's/ \\ / /g')
 
 if [ $MASTER_TYPE = "single" ]; then
     $(cat /vagrant/kubeadm-init.out | grep -A 2 "kubeadm join" | sed -e 's/^[ \t]*//' | tr '\n' ' ' | sed -e 's/ \\ / /g')
 else
     $(cat /vagrant/workers-join.out | sed -e 's/^[ \t]*//' | tr '\n' ' ' | sed -e 's/ \\ / /g')
 fi
-
-
 
 echo KUBELET_EXTRA_ARGS=--node-ip=$WORKER_IP > /etc/default/kubelet
