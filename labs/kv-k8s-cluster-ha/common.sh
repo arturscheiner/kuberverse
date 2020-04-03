974135b69ba33000edb1398c3fda6c00bcf68375
Artur Scheiner <artur.scheiner@gmail.com>
2020-04-03 20:31:25 -0300

    Added conntrack-tools-dbg

--- labs/kv-k8s-cluster-ha/common.sh
+++ labs/kv-k8s-cluster-ha/common.sh
@@ -30,7 +30,7 @@ add-apt-repository \
 
 apt-get update
 
-apt-get install -y avahi-daemon libnss-mdns traceroute htop httpie bash-completion ruby docker-ce=5:18.09.1~3-0~ubuntu-xenial kubeadm kubelet kubectl
+apt-get install -y conntrack-tools-dbg avahi-daemon libnss-mdns traceroute htop httpie bash-completion ruby docker-ce=5:18.09.1~3-0~ubuntu-xenial kubeadm kubelet kubectl
 
 modprobe nf_conntrack
 
