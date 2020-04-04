a003d6c33b57080b79ba3daa0c4b8c973f38b7e5
Artur Scheiner <artur.scheiner@gmail.com>
2020-04-04 15:58:10 -0300

    Adjusted haproxy.cfg

--- labs/kv-k8s-cluster-ha/scaler.sh
+++ labs/kv-k8s-cluster-ha/scaler.sh
@@ -27,25 +27,40 @@ apt-get update
 apt-get install -y avahi-daemon libnss-mdns traceroute htop httpie bash-completion haproxy ruby docker-ce
 
 cat >> /etc/haproxy/haproxy.cfg <<EOF
+global
+        log /dev/log    local0
+        log /dev/log    local1 notice
+        chroot /var/lib/haproxy
+        stats socket /run/haproxy/admin.sock mode 660 level admin expose-fd listeners
+        stats timeout 30s
+        user haproxy
+        group haproxy
+        daemon
+
+        ca-base /etc/ssl/certs
+        crt-base /etc/ssl/private
+
+        ssl-default-bind-ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384
+
 frontend kv-scaler
-    bind *:6443
-    mode tcp
-    log global
-    option tcplog
-    timeout client 3600s
-    backlog 4096
-    maxconn 50000
-    use_backend kv-masters
+        bind *:6443
+        mode tcp
+        log global
+        option tcplog
+        timeout client 3600s
+        backlog 4096
+        maxconn 50000
+        use_backend kv-masters
 
 backend kv-masters
-    mode  tcp
-    option log-health-checks
-    option redispatch
-    option tcplog
-    balance roundrobin
-    timeout connect 1s
-    timeout queue 5s
-    timeout server 3600s
+        mode  tcp
+        option log-health-checks
+        option redispatch
+        option tcplog
+        balance roundrobin
+        timeout connect 1s
+        timeout queue 5s
+        timeout server 3600s
 EOF
 
 i=0
