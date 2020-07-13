4cdc08c6d7db2a65c0a9dd130fba503b09ebc611
Ian Brown <ian@zestysoft.com>
2020-07-12 21:05:39 -0700

    Bugfixes and optimizations

--- labs/kv-k8s-cluster-ha/common.sh
+++ labs/kv-k8s-cluster-ha/common.sh
@@ -9,34 +9,34 @@ KVMSG=$1
 NODE_ADDRESS=$2
 MASTER_TYPE=$3
 
+UBUNTU_CODENAME=$(lsb_release -cs)
+
+export DEBIAN_FRONTEND=noninteractive
+
 ### Install packages to allow apt to use a repository over HTTPS
-apt-get update && apt-get install apt-transport-https ca-certificates curl software-properties-common
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
 ### Add Kubernetes GPG key
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
 ### Kubernetes Repo
-echo "deb  http://apt.kubernetes.io/  kubernetes-xenial  main" > /etc/apt/sources.list.d/kubernetes.list
+add-apt-reposity "deb http://apt.kubernetes.io/ kubernetes-$UBUNTU_CODENAME main"
 
 ### Add Docker’s official GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
-### Add Docker apt repository.
-add-apt-repository \
-  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) \
-  stable"
+### Add Docker apt repository
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $UBUNTU_CODENAME stable"
 
-
+### Refresh apt cache
 apt-get update
 
-apt-get install -y nfs-kernel-server nfs-common avahi-daemon libnss-mdns traceroute htop httpie bash-completion ruby docker-ce=5:18.09.1~3-0~ubuntu-xenial kubeadm kubelet kubectl
-
-#modprobe nf_conntrack
+apt-get install -y nfs-kernel-server nfs-common avahi-daemon libnss-mdns traceroute htop httpie bash-completion ruby docker-ce=5:18.09.1~3-0~ubuntu-$UBUNTU_CODENAME kubeadm kubelet kubectl
 
 cat /vagrant/hosts.out >> /etc/hosts
 
-# Setup Docker daemon.
+# Setup Docker daemon
 cat > /etc/docker/daemon.json <<EOF
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
@@ -50,7 +50,7 @@ EOF
 
 mkdir -p /etc/systemd/system/docker.service.d
 
-# Restart docker.
+# Restart docker
 systemctl daemon-reload
 systemctl restart docker
 
