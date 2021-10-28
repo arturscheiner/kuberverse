de8f92863d8a6128d6ee5a00012c8fe76db3a660
Artur Scheiner <artur.scheiner@gmail.com>
2021-10-28 15:56:42 -0300

    Updating network configuration

--- labs/kv-k8s-cluster-ha/master.sh
+++ labs/kv-k8s-cluster-ha/master.sh
@@ -28,8 +28,8 @@ else
 
     if (( $NODE == 0 )) ; then
 
-        ip route del default
-        ip route add default via $MASTER_IP
+        #ip route del default
+        #ip route add default via $MASTER_IP
         kubeadm init --control-plane-endpoint "kv-scaler.lab.local:6443" --upload-certs --pod-network-cidr $POD_CIDR --apiserver-cert-extra-sans kv-master.lab.local --apiserver-cert-extra-sans kv-scaler.lab.local | tee /vagrant/kubeadm-init.out
 
         k=$(grep -n "kubeadm join kv-scaler.lab.local" /vagrant/kubeadm-init.out | cut -f1 -d:)
@@ -38,8 +38,8 @@ else
         awk -v ln=$x 'NR>=ln && NR<=ln+1' /vagrant/kubeadm-init.out | tee /vagrant/workers-join.out
 
     else
-        ip route del default
-        ip route add default via $MASTER_IP
+        #ip route del default
+        #ip route add default via $MASTER_IP
         $(cat /vagrant/masters-join.out | sed -e 's/^[ \t]*//' | tr '\n' ' ' | sed -e 's/ \\ / /g')
     fi
 
@@ -60,5 +60,7 @@ if (( $NODE == 0 )) ; then
 fi
 
 echo KUBELET_EXTRA_ARGS=--node-ip=$MASTER_IP  > /etc/default/kubelet
+#ip route del default
+#ip route add default via 10.0.2.2
 systemctl restart networking
 systemctl restart kubelet
