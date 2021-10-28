746f2ff5f403f65c4e0ef60572489e6a0ef0150f
Artur Scheiner <artur.scheiner@gmail.com>
2021-10-28 14:41:22 -0300

    Update config

--- labs/kv-k8s-cluster-ha/master.sh
+++ labs/kv-k8s-cluster-ha/master.sh
@@ -30,7 +30,7 @@ else
 
         #ip route del default
         #ip route add default via $MASTER_IP
-        kubeadm init --control-plane-endpoint "kv-scaler.lab.local:6443" --upload-certs --pod-network-cidr $POD_CIDR --apiserver-cert-extra-sans kv-master.lab.local kv-scaler.lab.local | tee /vagrant/kubeadm-init.out
+        kubeadm init --control-plane-endpoint "kv-scaler.lab.local:6443" --upload-certs --pod-network-cidr $POD_CIDR --apiserver-cert-extra-sans kv-master.lab.local --apiserver-cert-extra-sans kv-scaler.lab.local | tee /vagrant/kubeadm-init.out
 
         k=$(grep -n "kubeadm join kv-scaler.lab.local" /vagrant/kubeadm-init.out | cut -f1 -d:)
         x=$(echo $k | awk '{print $1}')
